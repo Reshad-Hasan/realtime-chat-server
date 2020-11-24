c7b808a9a896e895004ae89b71896d0e601ef714
Reshad Hasan <reshad.kinetik.care>
2020-11-24 17:40:14 +0600

    initialized socket.io instance for new connection

--- index.js
+++ index.js
@@ -5,11 +5,19 @@ const http = require("http");
 
 const PORT = process.env.PORT || 5000;
 
-const router = require('./router');
+const router = require("./router");
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
+
+io.on("connection", (socket) => {
+  console.log("New user connected!!!");
+
+  socket.on("disconnect", () => {
+    console.log("User had left!!!");
+  });
+});
 
 app.use(router);
 
