1b555322450d5fd6ff9739f1a90645cb2db45a81
Reshad Hasan <reshad.kinetik.care>
2020-11-27 17:25:37 +0600

    adding users receiving and sending messages in a room

--- index.js
+++ index.js
@@ -6,13 +6,32 @@ const http = require("http");
 const PORT = process.env.PORT || 5000;
 
 const router = require("./router");
+const { addUser, removeUser, getUser, getUsersInRoom } = require("./user");
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
 io.on("connection", (socket) => {
-  console.log("New user connected!!!");
+  socket.on("join", ({ name, room }, cb) => {
+    const { error, user } = addUser(socket.id, name, room);
+    if (error)
+      return cb( error );
+
+    socket.emit("message", { user: "admin", text: `${user.name} welcome to this room` });
+    socket.broadcast.to(user.room).emit("message", { user: "admin", text: `${user.name} has joined` });
+    socket.join(user.room);
+
+    cb();
+  });
+
+  socket.on('sendMessage', (message, cb) => {
+    const user = getUser(socket.id);
+
+    io.to(user.room).emit('message', {user: user.name, text: message});
+
+    cb();
+  })
 
   socket.on("disconnect", () => {
     console.log("User had left!!!");
