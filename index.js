2690836ae81204ea1653861a0e5fbd0cba28dc88
Reshad Hasan <reshad.kinetik.care>
2020-11-29 02:14:45 +0600

    sending roomData to client

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const socketio = require("socket.io");
+const socketIO = require("socket.io");
 
 const http = require("http");
 
@@ -10,31 +10,36 @@ const { addUser, removeUser, getUser, getUsersInRoom } = require("./user");
 
 const app = express();
 const server = http.createServer(app);
-const io = socketio(server);
+const io = socketIO(server);
 
 io.on("connection", (socket) => {
   socket.on("join", ({ name, room }, cb) => {
     const { error, user } = addUser(socket.id, name, room);
     if (error)
-      return cb( error );
+      return cb(error);
 
     socket.emit("message", { user: "admin", text: `${user.name} welcome to this room` });
     socket.broadcast.to(user.room).emit("message", { user: "admin", text: `${user.name} has joined` });
     socket.join(user.room);
 
+    io.to(user.room).emit("roomData", { room: user.room, users: getUsersInRoom(user.room) });
     cb();
   });
 
-  socket.on('sendMessage', (message, cb) => {
+  socket.on("sendMessage", (message, cb) => {
     const user = getUser(socket.id);
 
-    io.to(user.room).emit('message', {user: user.name, text: message});
+    io.to(user.room).emit("message", { user: user.name, text: message });
 
     cb();
-  })
+  });
 
   socket.on("disconnect", () => {
-    console.log("User had left!!!");
+    const user = removeUser(socket.id);
+    if (user) {
+      io.to(user.room).emit("message", { user: "admin", text: `${user.name} has left` });
+      io.to(user.room).emit("roomData", { room: user.room, users: getUsersInRoom(user.room) });
+    }
   });
 });
 
